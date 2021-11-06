6d063921583e2cd3c77f7c18e98eff00f6876e48
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-06 10:51:07 +0300

    Add Category controller & Index View

---
+++ ProductTrack/ProductTrack/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using ProductTrack.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductTrack.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ProductTrackEntities ProductTrackEntities = new ProductTrackEntities();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(ProductTrackEntities.TBL_Customers.ToList());
+        }
+    }
+}
