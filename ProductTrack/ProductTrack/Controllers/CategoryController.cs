e1120ad3c3f5ccc55a06b94c0fab1d235f827409
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-06 11:06:57 +0300

    Update CategoryController & category Index View

--- ProductTrack/ProductTrack/Controllers/CategoryController.cs
+++ ProductTrack/ProductTrack/Controllers/CategoryController.cs
@@ -9,12 +9,27 @@ namespace ProductTrack.Controllers
 {
     public class CategoryController : Controller
     {
-        ProductTrackEntities ProductTrackEntities = new ProductTrackEntities();
+        private ProductTrackEntities ProductTrackEntities = new ProductTrackEntities();
 
         // GET: Category
         public ActionResult Index()
         {
             return View(ProductTrackEntities.TBL_Categories.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AddNewCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewCateogry(TBL_Categories categories)
+        {
+            ProductTrackEntities.TBL_Categories.Add(categories);
+            ProductTrackEntities.SaveChanges();
+            
+            return View();
         }
     }
 }
